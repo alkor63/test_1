52c308c088a3ca9d770a712c7d3b16a7ef36eeda
alkor63 <alkor@medi-spa.ru>
2023-04-12 21:06:19 +0300

    исправил 3 и 4 тест

--- src/test/java/pro/sky/testofcode/UserTest.java
+++ src/test/java/pro/sky/testofcode/UserTest.java
@@ -4,34 +4,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static pro.sky.testofcode.User.*;
-
 class UserTest {
-    User out = new User("myLogin","test@email.ru");
+    User out = new User("myLogin", "test@email.ru");
 
     @Test
     @DisplayName("Проверка создания объекта new User с двумя параметрами")
     public void shouldReturnMessageWhenLoginAndEMailIsCorrect() {
-        Assertions.assertDoesNotThrow(() -> {new User("myLogin","test@email.ru");},"login & eMail введены корректно");
- }
+        Assertions.assertDoesNotThrow(() -> {
+            new User("myLogin", "test@email.ru");
+        }, "login & eMail введены корректно");
+    }
 
     @Test
     @DisplayName("Проверка создания объекта new User без параметров")
     public void shouldReturnMessageWhenLoginAndEMailIsAbsent() {
-        Assertions.assertDoesNotThrow(() -> {new User();},"login и eMail не введены");
+        Assertions.assertDoesNotThrow(() -> {
+            new User();
+        }, "login и eMail не введены");
     }
 
     @Test
-    @DisplayName("Проверка наличия в строке email символов @ и .")
-    public void shouldReturnTrueWhenEMailIsCorrect() {
+    @DisplayName("Проверка выбрасывания исключения, если в строке email нет символов @ и .")
+    public void shouldReturnTrowsWhenEMailIsIncorrect() {
         String result = out.getEmail();
-        Assertions.assertTrue(checkCorrectEMail(result), "некорректный значкние email: "+result);
-    }
-    @Test
-    @DisplayName("Проверка равенства строк login и email")
-    public void shouldReturnTrueWhenLoginEqualsEMail() {
-        String result = out.getEmail();
-        Assertions.assertFalse(checkEquals2Strings(result,out.getLogin()), "login & email = "+result);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new User("myLogin", "testemailru");
+        });
     }
 
+    @Test
+    @DisplayName("Проверка выбрасывания исключения, если login и email равны")
+    public void shouldReturnTrowsWhenLoginEqualsEMail() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new User("test@email.ru", "test@email.ru");
+        });
+    }
 }
