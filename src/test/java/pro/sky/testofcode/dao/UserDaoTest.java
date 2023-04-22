1679886dab2921bef6ea05e13a2bb28f58e8495e
alkor63 <alkor@medi-spa.ru>
2023-04-22 15:18:11 +0300

    3-я итерация

--- src/test/java/pro/sky/testofcode/dao/UserDaoTest.java
+++ src/test/java/pro/sky/testofcode/dao/UserDaoTest.java
@@ -2,40 +2,22 @@ package pro.sky.testofcode.dao;
 
 import org.junit.jupiter.api.*;
 import pro.sky.testofcode.dao.impl.UserDaoImpl;
-import pro.sky.testofcode.model.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
 
-private final UserDaoImpl out = new UserDaoImpl() {
+    private final UserDao out = new UserDaoImpl();
 
-    @Override
-    public User getUserByName(String name) {
-        return null;
-    }
-
-    @Override
-    public List<User> findAllUsers() {
-        return null;
-    }
-
-};
     @Test
     @DisplayName("Проверка метода getUserByName - поиск User по имени")
     public void shouldReturnMessageWhenNameExistInArrayList() {
-        assertDoesNotThrow(() -> {
-            out.getUserByName("Dima");
-        }, "Метод getUserByName с существующим именем Юзера работает корректно");
+        assertNotNull(out.getUserByName("Dima"));
     }
 
     @Test
     @DisplayName("Проверка метода getUserByName когда User с искомым именем отсутствует")
     public void shouldReturnMessageWhenLoginAndEMailIsAbsent() {
         assertNull(out.getUserByName("Vova"));
-        }
-
+    }
 }
